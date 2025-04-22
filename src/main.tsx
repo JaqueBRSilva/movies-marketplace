792285b00b46aae8960a68bf6c62d8f2a05ee080
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2025-04-22 20:00:47 -0300

    feat: criado rota p/ 'home'

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Header from './components/Header/index.tsx';
+import './index.css';
+import Home from './pages/home/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+
+    </BrowserRouter>
+  </StrictMode>
 )

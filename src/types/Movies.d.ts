58351570d285db64bb976af83cab775fb7c2096e
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2025-04-22 22:47:26 -0300

    feat: add type 'Movies'

---
+++ src/types/Movies.d.ts
@@ -0,0 +1,15 @@
+export interface MoviesProps {
+    id?: string
+    image: string
+    title: string
+    price: number
+}
+
+export interface IMoviesContext {
+    movies: MoviesProps[]
+    saveMovies: (movie: []) => void
+}
+
+export type MoviesProvider_Props = {
+    children: React.ReactNode
+}

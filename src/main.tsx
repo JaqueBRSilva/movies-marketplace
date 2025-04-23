f71d86ad88179be2c04c634b4829169bd6da35fe
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2025-04-22 21:01:49 -0300

    feat: add 'MoviesListContext'

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,21 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import Header from './components/Header/index.tsx';
+import MoviesProvider from './contexts/MoviesListContext.tsx';
 import './index.css';
 import Home from './pages/home/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Header />
+    <MoviesProvider>
+      <BrowserRouter>
+        <Header />
 
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
 
-    </BrowserRouter>
+      </BrowserRouter>
+    </MoviesProvider>
   </StrictMode>
 )

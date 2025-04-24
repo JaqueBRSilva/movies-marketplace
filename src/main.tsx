0cfd425cb810ebefef70fb880518739bec8f987c
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2025-04-23 23:51:29 -0300

    feat: importado providers dos context's

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
-import { StrictMode } from 'react';
+import { StrictMode, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import Header from './components/Header/index.tsx';
+import LoadingSpinner from './components/LoadingSpinner/index.tsx';
+import CartProvider from './contexts/CartItemsContext.tsx';
 import MoviesProvider from './contexts/MoviesListContext.tsx';
 import './index.css';
 import Home from './pages/home/index.tsx';
@@ -9,14 +11,19 @@ import Home from './pages/home/index.tsx';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MoviesProvider>
-      <BrowserRouter>
-        <Header />
+      <CartProvider>
 
-        <Routes>
-          <Route path='/' element={<Home />} />
-        </Routes>
+        <BrowserRouter>
+          <Header />
 
-      </BrowserRouter>
+          <Suspense fallback={<LoadingSpinner />}>
+            <Routes>
+              <Route path='/' element={<Home />} />
+            </Routes>
+          </Suspense>
+
+        </BrowserRouter>
+      </CartProvider>
     </MoviesProvider>
   </StrictMode>
 )
